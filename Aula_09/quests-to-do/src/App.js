d6b87f59cb08a0b064fa2a448e5cc4bb256888d1
André Souza <f203606@gmail.com>
2025-01-23 13:52:27 -0300

    CY3

---
+++ Aula_09/quests-to-do/src/App.js
@@ -0,0 +1,19 @@
+import AddQuest from "./AddQuest";
+
+function App() {
+  function minhaFuncao(title) {
+    console.log(title);
+  }
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="card w-[80%] lg:w-[50%] h-[70%] shadow-md rounded-sm transform ease-out duration-300 items-center p-10 gap-5">
+        <h1 className="text-5xl font-work font-bold w-fit text-center">
+          Quests To Do
+        </h1>
+        <AddQuest saveAddQuest={minhaFuncao} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Aula_09/quests-to-do/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+      work: ['"Times New Roman"', 'sans-serif'],
+    },
+  },
+  },
+  plugins: [],
+}
+
